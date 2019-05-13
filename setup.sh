da4d897a0063cb73893f442f7412b463c30a7ef7
gichanglee <ratsgo@naver.com>
2019-05-13 17:53:05 +0900

    [bash] #5 추가 외부 라이브러리 반영

--- setup.sh
+++ setup.sh
@@ -16,12 +16,9 @@ pip install soynlp
 pip install konlpy
 bash <(curl -s https://raw.githubusercontent.com/konlpy/konlpy/master/scripts/mecab.sh)
 
-# glove
-pip install glove-python
-
 # khai
 cd ~/works
-brew install cmake
+# brew install cmake
 git clone https://github.com/kakao/khaiii.git
 cd khaiii
 mkdir build
@@ -34,4 +31,23 @@ ctest
 make package_python
 pip install  .
 
+# sentence piece
+cd ~/works
+git clone https://github.com/google/sentencepiece.git
+cd sentencepiece
+mkdir build
+cmake ..
+make -j $(nproc)
+sudo make install
+# sudo ldconfig -v # except macOS
+sudo update_dyld_shared_cache # macOS
+
+# glove
+cd ~/works
+git clone http://github.com/stanfordnlp/glove
+cd glove && make
+
 # fasttext
+cd ~/works
+git clone https://github.com/facebookresearch/fastText.git
+cd fastText && make
