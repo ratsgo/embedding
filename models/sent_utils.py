1e928aaefb9b62babe8935d1f810094eef99d273
gichanglee <ratsgo@naver.com>
2019-05-15 22:09:15 +0900

    [Python] #6 nsmc movie id, ELMo 반영

--- models/sent_utils.py
+++ models/sent_utils.py
@@ -1,12 +1,21 @@
-import sys
+import sys, glob, json, re
+from collections import Counter
 from gensim import corpora
 from gensim.models import Doc2Vec, ldamulticore
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.decomposition import TruncatedSVD
+from konlpy.tag import Mecab
+from gensim.models.doc2vec import TaggedDocument
 
 
 def latent_semantic_analysis(corpus_fname, output_fname):
-    corpus = [sent.replace('\n', '').strip() for sent in open(corpus_fname, 'r').readlines()]
+    corpus = []
+    movie_ids = []
+    with open(corpus_fname, 'r', encoding='utf-8') as f:
+        for line in f:
+            tokens, movie_id = line.replace('\n', '').strip().split("\u241E")
+            corpus.append(tokens)
+            movie_ids.extend(movie_id)
     # construct tf-idf matrix
     vectorizer = TfidfVectorizer(
         min_df=10,
@@ -20,18 +29,36 @@ def latent_semantic_analysis(corpus_fname, output_fname):
     with open(output_fname, 'w') as f:
         for doc_idx, vec in enumerate(vecs):
             str_vec = [str(el) for el in vec]
-            f.writelines(str(doc_idx) + ' ' + ' '.join(str_vec) + "\n")
+            f.writelines(str(doc_idx) + ' ' + str(movie_ids[doc_idx]) + ' '.join(str_vec) + "\n")
+
+
+class Doc2VecInput:
+
+    def __init__(self, fname):
+        self.fname = fname
+
+    def __iter__(self):
+        with open(self.fname, encoding='utf-8') as f:
+            for line in f:
+                tokens, movie_id = line.replace('\n', '').strip().split("\u241E")
+                tagged_doc = TaggedDocument(words=tokens.split(), tags=['MOVIE_%s' % movie_id])
+                yield tagged_doc
 
 
 def doc2vec(corpus_fname, output_fname):
-    # TODO: doc2vec용 코퍼스 찾고 작동하게 만들기
-    corpus = [sent.replace('\n', '').strip().split(" ") for sent in open(corpus_fname, 'r').readlines()]
+    corpus = Doc2VecInput(corpus_fname)
     model = Doc2Vec(corpus)
     model.save(output_fname)
 
 
 def latent_dirichlet_allocation(corpus_fname, output_fname):
-    raw_corpus = [sent.replace('\n', '').strip().split(" ") for sent in open(corpus_fname, 'r').readlines()]
+    raw_corpus = []
+    movie_ids = []
+    with open(corpus_fname, 'r', encoding='utf-8') as f:
+        for line in f:
+            tokens, movie_id = line.replace('\n', '').strip().split("\u241E")
+            raw_corpus.append(tokens.split(" "))
+            movie_ids.extend(movie_id)
     dictionary = corpora.Dictionary(raw_corpus)  # token : idx
     corpus = [dictionary.doc2bow(text) for text in raw_corpus]  # construct DTM, (token_id, freq)
     LDA = ldamulticore.LdaMulticore(corpus, id2word=dictionary,
@@ -42,8 +69,44 @@ def latent_dirichlet_allocation(corpus_fname, output_fname):
         for doc_idx, doc in enumerate(corpus):
             # TODO : topic dist가 모두 uniform하게 나오는 현상 해결
             doc_topic_distribution = [str(el[1]) for el in LDA[doc]]
-            f.writelines(str(doc_idx) + ' ' + ' '.join(doc_topic_distribution) + "\n")
+            f.writelines(str(doc_idx) + ' ' + str(movie_ids[doc_idx]) + ' ' + ' '.join(doc_topic_distribution) + "\n")
     LDA.save(output_fname + ".model")
+
+
+def construct_elmo_vocab(corpus_fname, output_fname):
+    count = Counter()
+    with open(corpus_fname, 'r', encoding='utf-8') as f1:
+        for sentence in f1:
+            tokens = sentence.replace('\n', '').strip().split(" ")
+            for token in tokens:
+                count[token] += 1
+    with open(output_fname, 'w', encoding='utf-8') as f2:
+        f2.writelines("</S>\n")
+        f2.writelines("<S>\n")
+        f2.writelines("<UNK>\n")
+        for word, _ in count.most_common(100000):
+            f2.writelines(word + "\n")
+
+
+def process_nsmc(corpus_dir, output_fname):
+    tokenizer = Mecab()
+    file_paths = glob.glob(corpus_dir + "/*")
+    with open(output_fname, 'w', encoding='utf-8') as f:
+        for path in file_paths:
+            contents = json.load(open(path))
+            for content in contents:
+                tokens = tokenizer.morphs(content['review'])
+                tokenized_sent = ' '.join(post_processing(tokens))
+                f.writelines(tokenized_sent + "\u241E" + content['movie_id'] + "\n")
+
+
+def post_processing(tokens):
+    results = []
+    for token in tokens:
+        # 숫자에 공백을 주어서 띄우기
+        processed_token = [el for el in re.sub(r"(\d)", r" \1 ", token).split(" ") if len(el) > 0]
+        results.extend(processed_token)
+    return results
 
 
 if __name__ == '__main__':
@@ -56,3 +119,7 @@ if __name__ == '__main__':
         doc2vec(in_f, out_f)
     elif util_mode == "latent_dirichlet_allocation":
         latent_dirichlet_allocation(in_f, out_f)
+    elif util_mode == "construct_elmo_vocab":
+        construct_elmo_vocab(in_f, out_f)
+    elif util_mode == "process_nsmc":
+        process_nsmc(in_f, out_f)
--- sentmodel.sh
+++ sentmodel.sh
@@ -3,15 +3,42 @@
 COMMAND=$1
 
 case $COMMAND in
+    process-nsmc)
+        echo ".."
+        python models/sent_utils.py process_nsmc /Users/david/works/nsmc/raw data/review_movieid.txt
+        ;;
     lsa-tfidf)
         echo "latent semantic analysis with tf-idf matrix..."
-        python models/sent_utils.py latent_semantic_analysis data/tmp.txt data/lsa-tfidf.vecs
+        python models/sent_utils.py latent_semantic_analysis data/review_movieid.txt data/lsa-tfidf.vecs
         ;;
     doc2vec)
         echo "doc2vec..."
-        python models/sent_utils.py doc2vec data/tmp.txt data/doc2vec.vecs
+        python models/sent_utils.py doc2vec data/review_movieid.txt data/doc2vec.vecs
         ;;
     lda)
         echo "latent_dirichlet_allocation..."
-        python models/sent_utils.py latent_dirichlet_allocation data/tmp.txt data/lda
+        python models/sent_utils.py latent_dirichlet_allocation data/review_movieid.txt data/lda
+        ;;
+    train-elmo)
+        echo "train ELMo..."
+        python models/sent_utils.py construct_elmo_vocab data/corpus_mecab.txt data/elmo-vocab.txt
+        # options.json (small)
+        wget https://s3-us-west-2.amazonaws.com/allennlp/models/elmo/2x1024_128_2048cnn_1xhighway/elmo_2x1024_128_2048cnn_1xhighway_options.json
+        mv elmo_2x1024_128_2048cnn_1xhighway_options.json data/elmo-options.json
+        scp -P 30800 models/train_elmo.py ratsgo@112.217.184.162:~/bilm-tf
+        scp -P 30800 data/corpus_mecab.txt ratsgo@112.217.184.162:~/data
+        scp -P 30800 data/elmo-vocab.txt ratsgo@112.217.184.162:~/data
+        scp -P 30800 data/elmo-options.json ratsgo@112.217.184.162:~/data
+        # @workstation
+        mkdir data/traindata
+        split -l 20000 data/corpus_mecab.txt data/traindata/data_
+        cd ~/bilm-tf
+        source ~/tf120/bin/activate
+        export CUDA_VISIBLE_DEVICES=0,1
+        export LC_CTYPE=C.UTF-8
+        python3.6 train_elmo.py \
+            --train_prefix='/home/ratsgo/data/traindata/*' \
+            --vocab_file /home/ratsgo/data/elmo-vocab.txt \
+            --save_dir /home/ratsgo/elmo-model
+        ;;
 esac
