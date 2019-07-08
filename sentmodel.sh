54bb420ce17fa9c3dac1051deae3740215b9d5be
gichanglee <ratsgo@naver.com>
2019-07-08 13:04:58 +0900

    [bash] #19 xlnet pretrain 스크립트 수정

--- sentmodel.sh
+++ sentmodel.sh
@@ -190,7 +190,7 @@ case $COMMAND in
             --vocab_path /notebooks/embedding/data/sentence-embeddings/xlnet/pretrain-ckpt/sentence_model_sp10m.cased.v3
         echo "preprocess corpus..."
         cd models/xlnet
-        python data_utils.py --bsz_per_host=32 \
+        python data_utils.py --bsz_per_host=16 \
 	                         --num_core_per_host=1 \
 	                         --seq_len=512 \
 	                         --reuse_len=256 \
@@ -204,7 +204,7 @@ case $COMMAND in
 	                         --num_predict=85
 	    python train_gpu.py --record_info_dir=/notebooks/embedding/data/sentence-embeddings/xlnet/pretrain-ckpt/tfrecords \
                             --model_dir=/notebooks/embedding/data/sentence-embeddings/xlnet/pretrain-ckpt \
-                            --train_batch_size=32 \
+                            --train_batch_size=16 \
                             --seq_len=512 \
                             --reuse_len=256 \
                             --mem_len=384 \
