97fa0f6e411ed5adf8c7af660d4cdf6e80678abd
gichanglee <ratsgo@naver.com>
2019-09-08 11:47:42 +0900

    [python] #14 attention layer 보완

--- models/tune_utils.py
+++ models/tune_utils.py
@@ -119,7 +119,7 @@ def make_elmo_graph(options_fname, pretrain_model_fname, max_characters_per_toke
 
     # Attention Layer
     output_fw, output_bw = lstm_output
-    H = tf.contrib.layers.fully_connected(inputs=output_fw + output_fw, num_outputs=256, activation_fn=tf.nn.tanh)
+    H = tf.contrib.layers.fully_connected(inputs=output_fw + output_bw, num_outputs=256, activation_fn=tf.nn.tanh)
     attention_score = tf.nn.softmax(tf.contrib.layers.fully_connected(inputs=H, num_outputs=1, activation_fn=None))
     attention_output = tf.squeeze(tf.matmul(tf.transpose(H, perm=[0, 2, 1]), attention_score), axis=-1)
     layer_output = tf.nn.dropout(attention_output, dropout_keep_prob)
@@ -208,7 +208,7 @@ def make_word_embedding_graph(num_labels, vocab_size, embedding_size, tune=False
 
     # Attention Layer
     output_fw, output_bw = lstm_output
-    H = tf.contrib.layers.fully_connected(inputs=output_fw + output_fw, num_outputs=256, activation_fn=tf.nn.tanh)
+    H = tf.contrib.layers.fully_connected(inputs=output_fw + output_bw, num_outputs=256, activation_fn=tf.nn.tanh)
     attention_score = tf.nn.softmax(tf.contrib.layers.fully_connected(inputs=H, num_outputs=1, activation_fn=None))
     attention_output = tf.squeeze(tf.matmul(tf.transpose(H, perm=[0, 2, 1]), attention_score), axis=-1)
     layer_output = tf.nn.dropout(attention_output, dropout_keep_prob)
